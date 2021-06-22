78cfac2709222d2ab8bc918627012a98d7434fe6
Spoorti-08 <kspoorti08@gmail.com>
2021-06-22 15:05:49 +0530

    removed password

--- myProject/settings.py
+++ myProject/settings.py
@@ -142,7 +142,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'kspoorti08@gmail.com'
-EMAIL_HOST_PASSWORD = 'sputi625234'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 
